5965d5ede22427f70b3432781bfc5bb5d26317f2
Marius <marius.pocev@gmail.com>
2021-02-22 22:19:58 +0200

    Add full JSON schema generating code

--- index.js
+++ index.js
@@ -5,57 +5,37 @@ const Ajv = require("ajv").default;
 
 const graderConfigSchema = {
     'type': 'array',
-    'items': { // This should be anyof and then specify each configuration for each grader
+    'items': {
         'type': 'object',
         'properties': {
-            'name': { 'type': 'string' },
-            'weight': { 'type': 'integer' },
-            'condition': { 'type': 'integer' },
+            'name': {
+                'type': 'string',
+                'required': true
+            },
+            'weight': {
+                'type': 'integer',
+                'required': true
+            },
+            'condition': {
+                'type': 'integer',
+                'required': true
+            },
             'context': { 'type': 'string' },
             'depends-on': {
                 'type': 'array',
                 'items': { 'type': 'string' }
-            },
-            'configuration': { // This should allow multiple types
-                // 'type': 'object',
-                // 'properties': {
-                //     'test_files': {
-                //         'type': 'object',
-                //         'properties': {
-                //             'repository': {
-                //                 'type': 'string'
-                //             },
-                //             'branch': {
-                //                 'type': 'string'
-                //             },
-                //             'sha': {
-                //                 'type': 'string'
-                //             }
-                //         },
-                //         'required': [
-                //             'repository',
-                //             'branch',
-                //             'sha'
-                //         ]
-                //     },
-                //     'timeout': {
-                //         'type': 'string'
-                //     }
-                // },
-                // 'required': [
-                //     'test_files',
-                //     'timeout'
-                // ]
             }
         },
-        // 'required': [
-        //     'name',
-        //     'weight',
-        //     'condition',
-        //     'context',
-        //     'depends-on',
-        //     'configuration'
-        // ]
+        "allOf": [
+            {
+                "if": {
+                    "properties": { "name": { "const": "junit-grader" } }
+                },
+                "then": {
+                    "properties": { "configuration": { "pattern": "[0-9]{5}(-[0-9]{4})?" } }
+                }
+            },
+        ]
     }
 };
 
@@ -97,7 +77,18 @@ const schemaPropertyToJsonSchema = (property) => {
     }
     
     throw new Error(`Invalid grader config parameter type: ${type}`);
-}
+};
+
+const graderUrls = {
+    'javac-tool': 'http://192.168.99.1:3003/config_schema',
+    // 'rng-tool': 'http://192.168.99.1:3001/config_schema',
+    // 'config-tool': 'http://192.168.99.1:3002/config_schema',
+    'io-grader': 'http://192.168.99.1:3004/config_schema',
+    'junit-grader': 'http://192.168.99.1:3006/config_schema',
+    'cpp-iograder': 'http://192.168.99.1:3008/config_schema',
+    'cpp-compilation': 'http://192.168.99.1:3007/config_schema',
+    'cppunit-grader': 'http://192.168.99.1:3015/config_schema'
+};
 
 // Iterate through the properties of the grader config schema. For each
 // property, convert it to a JSON schema definition. Then reduce all of this
@@ -112,13 +103,62 @@ const convertConfigurableConfigSchemaToJsonSchema = (configSchema) => {
     return propertiesJsonSchema;
 };
 
+const configSchemaToJsonSchema = (configSchema) => {
+    if (configSchema.parameters == 0) {
+        // Grader is non-configurable
+        return {}
+    } else {
+        // Grader is configurable
+        return {
+            type: 'object',
+            properties: convertConfigurableConfigSchemaToJsonSchema(configSchema.parameters),
+            required: true
+        }
+    }
+};
+
+// Generates a JSON schema conditional statement for a configuration based on the name of the grader used
+const generateGraderConfigConditional = (graderName, configurationSchema) => {
+    return {
+        "if": {
+            "properties": { "name": { "const": graderName } }
+        },
+        "then": {
+            "properties": { "configuration": configurationSchema }
+        }
+    };
+}
+
+// Generates an 'allOf' array of JSON schema conditional statements to change
+// the 'configuration' property based on name of the grader
+const generateGraderConfigConditionals = async (graderUrls) => {
+    let graderConditionals = [];
+    for ([graderName, graderUrl] of Object.entries(graderUrls)) {
+        graderConditionals = [
+            generateGraderConfigConditional(
+                graderName,
+                configSchemaToJsonSchema(
+                    await retrieveGraderConfigSchema(graderUrl)
+                )
+            ),
+            ...graderConditionals,
+        ];
+    }
+    return graderConditionals;
+};
+
+const injectAllOf = (allOfArr) => {
+    graderConfigSchema.items.allOf = allOfArr;
+    return graderConfigSchema;
+}
+
 const retrieveGraderConfigSchema = async (endpoint) => {
     const response = await fetch(endpoint)
         .catch(error => { throw new Error(`Invalid request to server: ${error.message}`) });
 
     if (!response.ok) {
         const errorText = response.statusText;
-        throw new Error(`Invalid response from server: ${errorText}`);
+        throw new Error(`Invalid response from server ${endpoint}: ${errorText}`);
     }
 
     const responseJson = await response.json();
@@ -127,32 +167,35 @@ const retrieveGraderConfigSchema = async (endpoint) => {
 
 (async () => {
     try {
-        // const yamlFile = 'grader-config.yml';
-        // const convertedFile = yaml.load(fs.readFileSync(yamlFile, 'utf8'));
-        // console.log('YAML converted to JSON:');
-        // console.log(convertedFile);
+      // const graderSchemaEndpoint = "http://192.168.99.1:3006/config_schema";
+      // const graderSchema = await retrieveGraderConfigSchema(graderSchemaEndpoint);
+      // console.log(graderSchema);
 
-        // const ajv = new Ajv(); // options can be passed
-        // const validate = ajv.compile(graderConfigSchema);
-        // const valid = validate(convertedFile);
-        // if (!valid) {
-        //     console.log(validate.errors);
-        // } else {
-        //     console.log('Wow, that is incredibly valid')
-        // }
+      // const graderJsonSchema = configSchemaToJsonSchema(graderSchema);
+      // console.dir(graderJsonSchema, { depth: null });
 
-        const graderSchemaEndpoint = "http://192.168.99.1:3003/config_schema";
-        const graderSchema = await retrieveGraderConfigSchema(graderSchemaEndpoint);
+      // const conditional = generateGraderConfigConditional("junit-grader", graderJsonSchema);
+      // console.dir(conditional, { depth: null });
 
-        if (configSchema.parameters == 0) {
-            // Grader is non-configurable
-        } else {
-            // Grader is configurable
-        }
-        console.log(graderSchema);
+      const allOf = await generateGraderConfigConditionals(graderUrls);
+      // console.dir(allOf, { depth: null });
 
-        const graderJsonSchema = convertConfigSchemaToJsonSchema(graderSchema.parameters);
-        console.dir(graderJsonSchema, { depth: null });
+      const finalSchema = injectAllOf(allOf);
+      console.dir(finalSchema, { depth: null });
+      
+      // const yamlFile = 'grader-config.yml';
+      // const convertedFile = yaml.load(fs.readFileSync(yamlFile, 'utf8'));
+      // console.log('YAML converted to JSON:');
+      // console.log(convertedFile);
+
+      // const ajv = new Ajv(); // options can be passed
+      // const validate = ajv.compile(graderConfigSchema);
+      // const valid = validate(convertedFile);
+      // if (!valid) {
+      //     console.log(validate.errors);
+      // } else {
+      //     console.log('Wow, that is incredibly valid')
+      // }
     } catch (error) {
         console.log(error);
     }
