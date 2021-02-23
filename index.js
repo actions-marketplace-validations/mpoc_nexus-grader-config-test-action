7709a82d8e92cf16e6af9b012e88d7b8096df532
Marius <marius.pocev@gmail.com>
2021-02-23 18:09:03 +0200

    Fix grader config json schema generation

--- index.js
+++ index.js
@@ -3,82 +3,6 @@ const yaml = require('js-yaml');
 const fetch = require('node-fetch');
 const Ajv = require("ajv").default;
 
-const graderConfigSchema = {
-    'type': 'array',
-    'items': {
-        'type': 'object',
-        'properties': {
-            'name': {
-                'type': 'string',
-                'required': true
-            },
-            'weight': {
-                'type': 'integer',
-                'required': true
-            },
-            'condition': {
-                'type': 'integer',
-                'required': true
-            },
-            'context': { 'type': 'string' },
-            'depends-on': {
-                'type': 'array',
-                'items': { 'type': 'string' }
-            }
-        },
-        "allOf": [
-            {
-                "if": {
-                    "properties": { "name": { "const": "junit-grader" } }
-                },
-                "then": {
-                    "properties": { "configuration": { "pattern": "[0-9]{5}(-[0-9]{4})?" } }
-                }
-            },
-        ]
-    }
-};
-
-// Converts a parameter in a grader config schema to an equivalent JSON schema object
-const schemaPropertyToJsonSchema = (property) => {
-    if (property.type == 'git') {
-        return {
-            type: 'object',
-            properties: {
-                repository: {
-                    type: 'string',
-                    required: true
-                },
-                branch: {
-                    type: 'string',
-                    required: true
-                },
-                sha: {
-                    type: 'string',
-                    required: true
-                }
-            }
-        }
-    } else if (property.type == 'int') {
-        return {
-            type: 'int',
-            minimum: property.min,
-            maximum: property.max,
-            multipleOf: property.step,
-            default: property.initial,
-            required: true
-        }
-    } else if (property.type == 'string') {
-        return {
-            type: 'string',
-            default: property.initial,
-            required: true,
-        }
-    }
-    
-    throw new Error(`Invalid grader config parameter type: ${type}`);
-};
-
 const graderUrls = {
     'javac-tool': 'http://192.168.99.1:3003/config_schema',
     // 'rng-tool': 'http://192.168.99.1:3001/config_schema',
@@ -90,71 +14,67 @@ const graderUrls = {
     'cppunit-grader': 'http://192.168.99.1:3015/config_schema'
 };
 
-// Iterate through the properties of the grader config schema. For each
-// property, convert it to a JSON schema definition. Then reduce all of this
-// into a single object, which would represent the 'properties' object of some
-// JSON schema.
-const convertConfigurableConfigSchemaToJsonSchema = (configSchema) => {
-    const propertiesJsonSchema = Object.entries(configSchema).reduce((acc, [propertyName, schema]) => ({
-        [propertyName]: schemaPropertyToJsonSchema(schema), // Dynamic property name definition
-        ...acc
-    }), {});
-
-    return propertiesJsonSchema;
+const requiredAssignmentProperties = [ 'name', 'weight', 'condition', 'configuration' ];
+const graderConfigSchema = {
+    type: 'array',
+    items: {
+        type: 'object',
+        properties: {
+            name: { type: 'string', enum: Object.keys(graderUrls) },
+            weight: { type: 'integer' },
+            condition: { type: 'integer' },
+            context: { type: 'string' },
+            'depends-on': {
+                type: 'array',
+                items: { type: 'string' }
+            },
+            configuration: {
+                oneOf: [
+                    { type: 'object' },
+                    { type: 'null' }
+                ]
+            }
+        },
+        required: requiredAssignmentProperties, // I don't think this is strictly necessary
+        additionalProperties: false
+    }
 };
 
-const configSchemaToJsonSchema = (configSchema) => {
-    if (configSchema.parameters == 0) {
-        // Grader is non-configurable
-        return {}
-    } else {
-        // Grader is configurable
+// Converts a parameter in a grader config schema to an equivalent JSON schema object
+const schemaPropertyToJsonSchema = (property) => {
+    if (property.type == 'git') {
         return {
             type: 'object',
-            properties: convertConfigurableConfigSchemaToJsonSchema(configSchema.parameters),
-            required: true
+            properties: {
+                repository: { type: 'string', },
+                branch: { type: 'string', },
+                sha: { type: 'string', }
+            },
+            required: [ 'repository', 'branch', 'sha' ],
+            additionalProperties: false
+        }
+    } else if (property.type == 'int') {
+        return {
+            type: 'integer',
+            minimum: property.min,
+            maximum: property.max,
+            multipleOf: property.step,
+            default: property.initial
+        }
+    } else if (property.type == 'string') {
+        return {
+            type: 'string',
+            default: property.initial
         }
     }
+    
+    throw new Error(`Invalid grader config parameter type: ${type}`);
 };
 
-// Generates a JSON schema conditional statement for a configuration based on the name of the grader used
-const generateGraderConfigConditional = (graderName, configurationSchema) => {
-    return {
-        "if": {
-            "properties": { "name": { "const": graderName } }
-        },
-        "then": {
-            "properties": { "configuration": configurationSchema }
-        }
-    };
-}
-
-// Generates an 'allOf' array of JSON schema conditional statements to change
-// the 'configuration' property based on name of the grader
-const generateGraderConfigConditionals = async (graderUrls) => {
-    let graderConditionals = [];
-    for ([graderName, graderUrl] of Object.entries(graderUrls)) {
-        graderConditionals = [
-            generateGraderConfigConditional(
-                graderName,
-                configSchemaToJsonSchema(
-                    await retrieveGraderConfigSchema(graderUrl)
-                )
-            ),
-            ...graderConditionals,
-        ];
-    }
-    return graderConditionals;
-};
-
-const injectAllOf = (allOfArr) => {
-    graderConfigSchema.items.allOf = allOfArr;
-    return graderConfigSchema;
-}
-
-const retrieveGraderConfigSchema = async (endpoint) => {
+// Retrieve grader config schema from a GET config_schema endpoint
+const retrieveConfigSchema = async (endpoint) => {
     const response = await fetch(endpoint)
-        .catch(error => { throw new Error(`Invalid request to server: ${error.message}`) });
+        .catch(error => { throw new Error(`Invalid request to server ${endpoint}: ${error.message}`) });
 
     if (!response.ok) {
         const errorText = response.statusText;
@@ -163,39 +83,138 @@ const retrieveGraderConfigSchema = async (endpoint) => {
 
     const responseJson = await response.json();
     return responseJson;
-}
+};
+
+// Return the 'configuration' property for a grader in grader-config
+const configSchemaToJsonSchema = (configSchema) => {
+    if (configSchema.parameters == 0) {
+        // Grader is non-configurable
+        return { type: 'null' }
+        // return {
+        //     type: 'object',
+        //     properties: {},
+        //     additionalProperties: false
+        // }
+    } else {
+        // Grader is configurable
+        
+        const propertiesParam = {};
+        const properties = [];
+        for (const parameter in configSchema.parameters) {
+            // Generate the 'properties' property for a JSON schema object for
+            // each schema property received from a GET grader config schema
+            // endpoint
+            propertiesParam[parameter] = schemaPropertyToJsonSchema(configSchema.parameters[parameter]);
+
+            // Store each property in an array as a string. This will be used
+            // for the 'required' property for a JSON schema object.
+            properties.push(parameter);
+        }
+
+        return {
+            type: 'object',
+            properties: propertiesParam,
+            required: properties,
+            additionalProperties: false
+        }
+    }
+};
+
+// Generates a JSON schema conditional statement for a configuration based on the name of the grader used
+const configJsonSchemaToConditional = (graderName, configurationSchema) => {
+    // const configSchemaIsConfigurable = Object.keys(configurationSchema).length != 0;
+    // if (configSchemaIsConfigurable) {
+        return {
+            if: { properties: { name: { const: graderName } }, },
+            then: {
+                properties: { configuration: configurationSchema },
+                // required: ["configuration", ...requiredAssignmentProperties], // Assuming this replaces the required property that is already specified
+                required: requiredAssignmentProperties,
+                // additionalProperties: false
+            }
+        };
+    // } else {
+    //     return {
+    //         if: { properties: { name: { const: graderName } } },
+    //         then: {
+    //             properties: { configuration: { const: 0 } },
+    //             required: requiredAssignmentProperties,
+    //             // required: ["configuration", ...requiredAssignmentProperties], // Assuming this replaces the required property that is already specified
+    //             // additionalProperties: false
+    //         }
+    //     };
+    // }
+};
+
+// Generates an 'allOf' array of JSON schema conditional statements to change
+// the 'configuration' property based on name of the grader
+const generateConfigJsonSchemaConditionals = async (graderUrls) => {
+    const configJsonSchemas = {};
+    for (const graderName in graderUrls) {
+        const configSchema = await retrieveConfigSchema(graderUrls[graderName]);
+        const jsonSchema = configSchemaToJsonSchema(configSchema);
+        configJsonSchemas[graderName] = jsonSchema;
+    }
+
+    const configJsonSchemaConditionals = [];
+    for (const graderName in configJsonSchemas) {
+        const jsonSchemaConditional = configJsonSchemaToConditional(graderName, configJsonSchemas[graderName]);
+        configJsonSchemaConditionals.push(jsonSchemaConditional);
+    }
+
+    console.dir(configJsonSchemaConditionals, { depth: null })
+
+    return configJsonSchemaConditionals;
+};
+
+const injectConfigJsonSchemaConditionals = (configJsonSchemaConditionals) => {
+    graderConfigSchema.items.allOf = configJsonSchemaConditionals;
+};
 
 (async () => {
     try {
-      // const graderSchemaEndpoint = "http://192.168.99.1:3006/config_schema";
-      // const graderSchema = await retrieveGraderConfigSchema(graderSchemaEndpoint);
-      // console.log(graderSchema);
+    //   const configSchemaEndpoint = "http://192.168.99.1:3006/config_schema";
+    //   const configSchema = await retrieveConfigSchema(configSchemaEndpoint);
+    //   console.log(configSchema);
+    //   console.log();
+
+    //   const jsonSchema = configSchemaToJsonSchema(configSchema);
+    //   console.dir(jsonSchema, { depth: null });
+
+      const configJsonSchemaConditionals = await generateConfigJsonSchemaConditionals(graderUrls);
+      injectConfigJsonSchemaConditionals(configJsonSchemaConditionals);
+      console.dir(graderConfigSchema, { depth: null })
+      
+    //   const conditional = configurableGraderConfigJsonSchemaToConditional();
+    //   console.dir(conditional, { depth: null });
 
       // const graderJsonSchema = configSchemaToJsonSchema(graderSchema);
       // console.dir(graderJsonSchema, { depth: null });
 
-      // const conditional = generateGraderConfigConditional("junit-grader", graderJsonSchema);
+      // const conditional = configurableGraderConfigJsonSchemaToConditional("junit-grader", graderJsonSchema);
       // console.dir(conditional, { depth: null });
 
-      const allOf = await generateGraderConfigConditionals(graderUrls);
+    //   const allOf = await generateGraderConfigConditionals(graderUrls);
       // console.dir(allOf, { depth: null });
 
-      const finalSchema = injectAllOf(allOf);
-      console.dir(finalSchema, { depth: null });
-      
-      // const yamlFile = 'grader-config.yml';
-      // const convertedFile = yaml.load(fs.readFileSync(yamlFile, 'utf8'));
-      // console.log('YAML converted to JSON:');
-      // console.log(convertedFile);
+    //   const finalSchema = injectAllOf(allOf);
+    //   console.dir(finalSchema, { depth: 5 });
+    //   console.dir(graderConfigSchema, { depth: 16 });
 
-      // const ajv = new Ajv(); // options can be passed
-      // const validate = ajv.compile(graderConfigSchema);
-      // const valid = validate(convertedFile);
-      // if (!valid) {
-      //     console.log(validate.errors);
-      // } else {
-      //     console.log('Wow, that is incredibly valid')
-      // }
+      const yamlFile = 'grader-config.yml';
+      const convertedFile = yaml.load(fs.readFileSync(yamlFile, 'utf8'));
+      console.log('YAML converted to JSON:');
+      console.log(convertedFile);
+
+      const ajv = new Ajv(); // options can be passed
+      const validate = ajv.compile(graderConfigSchema);
+      const valid = validate(convertedFile);
+      if (!valid) {
+          console.log(validate.errors);
+      } else {
+          console.log('Wow, that is incredibly valid')
+      }
+
     } catch (error) {
         console.log(error);
     }
